3151d599ccd3407bd9a5276e823837aa5ab6f7cd
Nazmul Hasan Apu <nazmul.apu.hasan@gmail.com>
2017-05-30 22:47:17 +0300

    cart action on progress

---
+++ cartaction.php
@@ -0,0 +1,41 @@
+<?php
+include 'cart.php';
+$cart = new Cart;
+
+// Database config
+
+include 'dbConfig.php';
+if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){
+    if($_REQUEST['action'] == 'addToCart' && !empty($_REQUEST['id'])){
+        $productID = $_REQUEST['id'];
+       
+        $query = $db->query("SELECT * FROM products WHERE id = ".$productID);
+        $row = $query->fetch_assoc();
+        $itemData = array(
+            'id' => $row['id'],
+            'name' => $row['name'],
+            'price' => $row['price'],
+            'qty' => 1
+        );
+		
+		$insertItem = $cart->insert($itemData);
+        $redirectLoc = $insertItem?'viewCart.php':'index.php';
+        header("Location: ".$redirectLoc);
+    }elseif($_REQUEST['action'] == 'updateCartItem' && !empty($_REQUEST['id'])){
+        $itemData = array(
+            'rowid' => $_REQUEST['id'],
+            'qty' => $_REQUEST['qty']
+        );
+        $updateItem = $cart->update($itemData);
+        echo $updateItem?'ok':'err';die;
+    }elseif($_REQUEST['action'] == 'removeCartItem' && !empty($_REQUEST['id'])){
+        $deleteItem = $cart->remove($_REQUEST['id']);
+        header("Location: viewCart.php");
+    }elseif($_REQUEST['action'] == 'placeOrder' && $cart->total_items() > 0 && !empty($_SESSION['sessCustomerID'])){
+        
+        $insertOrder = $db->query("INSERT INTO orders (customer_id, total_price, created, modified) VALUES ('".$_SESSION['sessCustomerID']."', '".$cart->total()."', '".date("Y-m-d H:i:s")."', '".date("Y-m-d H:i:s")."')");
+		
+		
+
+
+
