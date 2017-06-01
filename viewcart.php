3f13f889baa9bdc6dd7ea648c7fd000050e081a5
Nazmul Hasan Apu <nazmul.apu.hasan@gmail.com>
2017-06-01 21:33:11 +0300

    viewcart on progress

---
+++ viewcart.php
@@ -0,0 +1,47 @@
+<?php
+
+include 'cart.php';
+$cart = new Cart;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>View Cart - PHP Shopping Cart Tutorial</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+    .container{padding: 50px;}
+    input[type="number"]{width: 20%;}
+    </style>
+    <script>
+    function updateCartItem(obj,id){
+        $.get("cartAction.php", {action:"updateCartItem", id:id, qty:obj.value}, function(data){
+            if(data == 'ok'){
+                location.reload();
+            }else{
+                alert('Cart update failed, please try again.');
+            }
+        });
+    }
+    </script>
+</head>
+<body>
+<div class="container">
+    <h1>Shopping Cart</h1>
+    <table class="table">
+    <thead>
+        <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody>
+</div> <!-- end of container -->
+</body>
+</html>
