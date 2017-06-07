7c41d38f4af8ddfb5a70472cc233301f411faef6
Nazmul Hasan Apu <nazmul.apu.hasan@gmail.com>
2017-06-07 20:51:08 +0300

    Cart view updated

--- viewcart.php
+++ viewcart.php
@@ -42,6 +42,36 @@ $cart = new Cart;
         </tr>
     </thead>
     <tbody>
+	<?php
+        if($cart->total_items() > 0){
+            //get cart items from session
+            $cartItems = $cart->contents();
+            foreach($cartItems as $item){
+        ?>
+        <tr>
+            <td><?php echo $item["name"]; ?></td>
+            <td><?php echo '$'.$item["price"].' USD'; ?></td>
+            <td><input type="number" class="form-control text-center" value="<?php echo $item["qty"]; ?>" onchange="updateCartItem(this, '<?php echo $item["rowid"]; ?>')"></td>
+            <td><?php echo '$'.$item["subtotal"].' USD'; ?></td>
+            <td>
+                <a href="cartAction.php?action=removeCartItem&id=<?php echo $item["rowid"]; ?>" class="btn btn-danger" onclick="return confirm('Are you sure?')"><i class="glyphicon glyphicon-trash"></i></a>
+            </td>
+        </tr>
+        <?php } }else{ ?>
+        <tr><td colspan="5"><p>Your cart is empty.....</p></td>
+        <?php } ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td><a href="index.php" class="btn btn-warning"><i class="glyphicon glyphicon-menu-left"></i> Continue Shopping</a></td>
+            <td colspan="2"></td>
+            <?php if($cart->total_items() > 0){ ?>
+            <td class="text-center"><strong>Total <?php echo '$'.$cart->total().' USD'; ?></strong></td>
+            <td><a href="checkout.php" class="btn btn-success btn-block">Checkout <i class="glyphicon glyphicon-menu-right"></i></a></td>
+            <?php } ?>
+        </tr>
+    </tfoot>
+    </table>
 </div> <!-- end of container -->
 </body>
 </html>
